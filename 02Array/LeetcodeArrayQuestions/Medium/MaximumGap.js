30ec27f9b685c03ffce30f9068aac44156bdf8d0
Janna007 <jannakondeth5@gmail.com>
2025-08-11 12:04:58 +0530

    problem solving

---
+++ 02Array/LeetcodeArrayQuestions/Medium/MaximumGap.js
@@ -0,0 +1,44 @@
+// Given an integer array nums, return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
+
+// You must write an algorithm that runs in linear time and uses linear extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,6,9,1]
+// Output: 3
+// Explanation: The sorted form of the array is [1,3,6,9], either (3,6) or (6,9) has the maximum difference 3.
+// Example 2:
+
+// Input: nums = [10]
+// Output: 0
+// Explanation: The array contains less than 2 elements, therefore return 0.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 0 <= nums[i] <= 109
+
+
+let nums=[10]
+var maximumGap = function(nums) {
+    nums.sort((a,b)=>a-b)
+    
+    let maxDiff=0
+    let i=0;
+    let j=1;
+
+    while(j<nums.length){
+        if(nums[j]-nums[i]>maxDiff){
+            maxDiff=nums[j]-nums[i]
+        }
+        j++;
+        i++;
+    }
+
+    return maxDiff
+}
+
+console.log(maximumGap(nums))
--- 06SortingAlgorithms/insertionSort.js
+++ 06SortingAlgorithms/insertionSort.js
@@ -1,11 +1,11 @@
 let arr=[0,3,2,6,7,1,3]
 let n=arr.length-1
 
-for(let i=1;i<=n;i++){
+for(let i=1;i<=n;i++){  //n
     let key=arr[i]
     let j=i-1;
 
-    while(j>=0 && arr[j]>key){
+    while(j>=0 && arr[j]>key){ 
       arr[j+1]=arr[j]
       j--;
     }
