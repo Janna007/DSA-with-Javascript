76e1e3a99b2f4e54ce96dac2cc4b16d29f740fdb
Janna007 <jannakondeth5@gmail.com>
2025-08-11 10:45:44 +0530

    sorting algorithms

---
+++ 06SortingAlgorithms/insertionSort.js
@@ -0,0 +1,17 @@
+let arr=[0,3,2,6,7,1,3]
+let n=arr.length-1
+
+for(let i=1;i<=n;i++){
+    let key=arr[i]
+    let j=i-1;
+
+    while(j>=0 && arr[j]>key){
+      arr[j+1]=arr[j]
+      j--;
+    }
+
+    arr[j+1]=key
+}
+
+
+console.log(arr)
